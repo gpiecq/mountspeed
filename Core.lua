7afb6306feee8db21f91727669e5d06163fc3c64
PIECQ Grégory <gregory.piecq@gmail.com>
2026-04-19 01:32:13 +0200

    Bump version to 1.0.0 and add CHANGELOG

--- Core.lua
+++ Core.lua
@@ -3,7 +3,7 @@
 -- Addon initialisation, SavedVariables defaults, event bus, slash cmds
 ----------------------------------------------------------------------
 local ADDON_NAME, NS = ...
-NS.version = "0.1.0"
+NS.version = "1.0.0"
 
 ----------------------------------------------------------------------
 -- Default saved-variables template (account-wide)
